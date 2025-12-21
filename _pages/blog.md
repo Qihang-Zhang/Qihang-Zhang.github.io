ea6e453a72a6958cb4e12ca2d00cd014073661ae
Qihang Zhang <qihangz.work@gmail.com>
2025-12-20 18:27:55 -0800

    Add .bundle-cache to .gitignore and create launch_local_server.sh script

--- _pages/blog.md
+++ _pages/blog.md
@@ -6,3 +6,8 @@ description:
 nav: true
 nav_order: 0
 ---
+
+<meta http-equiv="refresh" content="0; url=https://blog.qihang-zhang.com/">
+
+Redirecting to the blog… If it doesn't happen automatically, click
+[here](https://blog.qihang-zhang.com/).
---
+++ launch_local_server.sh
@@ -0,0 +1,17 @@
+docker run --rm -it \
+  -p 4000:4000 \
+  -v "$PWD":/site \
+  --user "$(id -u)":"$(id -g)" \
+  -e GEM_HOME=/tmp/gems \
+  -e GEM_PATH=/tmp/gems \
+  -e BUNDLE_PATH=/tmp/bundle \
+  -e BUNDLE_APP_CONFIG=/tmp/bundle-config \
+  --entrypoint bash \
+  bretfisher/jekyll-serve \
+  -lc 'set -e
+    mkdir -p "$GEM_HOME" "$BUNDLE_PATH" "$BUNDLE_APP_CONFIG"
+    gem install bundler -v 2.4.22
+    cd /site
+    bundle _2.4.22_ install
+    bundle _2.4.22_ exec jekyll serve --host 0.0.0.0 --port 4000 --trace
+  '
